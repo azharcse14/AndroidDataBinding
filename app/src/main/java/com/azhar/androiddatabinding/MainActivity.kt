863e00000a6734844f4aacbce2c8b0e001e9d8f7
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2021-10-03 23:31:09 +0600

    update

--- app/src/main/java/com/azhar/androiddatabinding/MainActivity.kt
+++ app/src/main/java/com/azhar/androiddatabinding/MainActivity.kt
@@ -2,13 +2,18 @@ package com.azhar.androiddatabinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.azhar.androiddatabinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding:Activ
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val quoteObj =  Quote("Duck Duck Go", "China")
+        binding.quote = quoteObj
     }
 }
---
+++ app/src/main/java/com/azhar/androiddatabinding/Quote.kt
@@ -0,0 +1,3 @@
+package com.azhar.androiddatabinding
+
+data class Quote(val text:String, val author:String)
